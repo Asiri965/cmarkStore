b69a7819cb1cc8fc9037a7d421c8dbdf00a86592
Asiri Mahesh <74152497+Asiri965@users.noreply.github.com>
2023-12-15 15:59:38 +0530

    First Commit

---
+++ customer/delete_account.php
@@ -0,0 +1,13 @@
+<center><!--center Begin -->
+    
+    <h1>Do You Really Want to Delete Your Account?</h1>
+    
+    <form action="" method="post"><!-- form Begin -->
+        
+        <input type="submit" name="Yes" value="Yes, I Want to Delete" class="btn btn-danger">
+        
+        <input type="submit" name="No" value="No, I Don't Want to Delete" class="btn btn-primary">
+        
+    </form><!-- form Finish -->
+    
+</center><!--center Finish -->
---
+++ customer/pay_offline.php
@@ -0,0 +1,43 @@
+<center><!-- center Begin -->
+    
+    <h1>Pay Offline Using Method</h1>
+    
+    <p class="text-muted">
+        
+        If you have any questions, feel free to <a href="../contact.php">contact us</a>. Our customer service work <strong>24/7</strong>
+        
+    </p>
+    
+</center><!-- center Finish -->
+
+
+<hr>
+
+
+<div class="table-responsive"><!-- table-responsive Begin -->
+    
+    <table class="table table-bordered table-hover table-stripped"><!-- table table-bordered table-hover Begin -->
+        
+        <thead><!-- thead Begin -->
+            
+            <tr><!-- tr Begin -->
+               
+               <th>Bank Account Details:</th>
+               <th>Easy Paisa, UBL, Ezcash/ mCash Details:</th>
+               <th>Western Union Details:</th>
+                
+            </tr><!-- tr Finish -->
+            
+        </thead><!-- thead Finish -->
+        
+        <tbody><!-- tbody Begin -->
+            
+           <td>Bank Name: HNB | Account No: 00445587266 | Branch Name: Kandana | Branch Code: 11320</td>
+           <td>NIC: 960652576V | Mobile No: 0774433253 | Name: Asiri</td>
+           <td>Real Name: Asiri Mahesh | Mobile No: 0774433253 | Country: Sri Lanka | Name: Asiri | NIC: 960652576V</td>
+            
+        </tbody><!-- tbody Finish -->
+        
+    </table><!-- table table-bordered table-hover Finish -->
+    
+</div><!-- table-responsive Finish -->
